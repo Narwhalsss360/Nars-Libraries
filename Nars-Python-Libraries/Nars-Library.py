81a26c0df3460d3d2f6895d70a02d699c6250da3
Franklyn <franklynvasquez24@gmail.com>
2021-10-25 02:10:25 -0400

    Added getImageFromUrl() to: python

--- Nars-Python-Libraries/Nars-Library.py
+++ Nars-Python-Libraries/Nars-Library.py
@@ -1,10 +1,15 @@
 from datetime import datetime
+from PIL import Image
+import requests
+from io import BytesIO
 
 class Logger():
-
 
     def __init__(self):
         pass
 
-
     pass
+
+def getImageFromUrl(url):
+    req = requests.get(url)
+    return Image.open(BytesIO(req.content))
