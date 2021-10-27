d0e1a9b94180d0300779845b2657a73ad8e6153f
Franklyn <franklynvasquez24@gmail.com>
2021-10-27 00:04:39 -0400

    KSP Test Successful

--- Testing/KSP NarsSerialCom Test/KSP NarsSerialComOpenNET Test.cs
+++ Testing/KSP NarsSerialCom Test/KSP NarsSerialComOpenNET Test.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using UnityEngine;
 using KSP.IO;
+using Nars_Libraries_Framework45;
 
 namespace KSP_NarsSerialComOpenNET_Test
 {
     [KSPAddon(KSPAddon.Startup.MainMenu, false)]
     public class KSP_NarsSerialComOpenNET_Test_Main
     {
+        Nars_Libraries_Framework45.Serial.NarsSerialComOpenNET port = new Nars_Libraries_Framework45.Serial.NarsSerialComOpenNET();
         void Awake()
         {
             Debug.Log("Compiled and ran!");
+            port.connect("\\\\.\\COM28");
         }
     }
 }
