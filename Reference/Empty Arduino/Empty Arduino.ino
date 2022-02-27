b8b22340fe90caff13beaa02791e4c927120865b
Franklyn Vasquez <franklynvasquez24@gmail.com>
2022-02-27 17:18:12 -0500

    After merge testing done, for .NET libararies and Arduino libraries only

--- Reference/Empty Arduino/Empty Arduino.ino
+++ Reference/Empty Arduino/Empty Arduino.ino
@@ -1,15 +1,11 @@
-/*
- Name:		Empty_Arduino.ino
- Created:	10/26/2021 9:08:34 PM
- Author:	sandr
-*/
+#include <Wire.h>
 
-// the setup function runs once when you press reset or power the board
-void setup() {
+void setup() 
+{
 
 }
 
-// the loop function runs over and over again until power down or reset
-void loop() {
+void loop() 
+{
   
 }
