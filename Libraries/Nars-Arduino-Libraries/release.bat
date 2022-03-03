30f495d3fa5ab74cd6d926d14c24d22fccab7b9d
Franklyn Vasquez <franklynvasquez24@gmail.com>
2022-03-03 01:28:21 -0500

    Added Arduino Library examples, few modifications, new literals, release.bat copy, .zip library, added liquid crystal to reference

---
+++ Libraries/Nars-Arduino-Libraries/release.bat
@@ -0,0 +1,4 @@
+echo d | xcopy /d /y .\src\keywords.txt .\NarsLibraries
+echo d | xcopy /d /y .\src\NarsLibraries.cpp .\NarsLibraries
+echo d | xcopy /d /y .\src\NarsLibraries.h .\NarsLibraries
+echo d | xcopy /d /y /s .\examples .\NarsLibraries\examples
